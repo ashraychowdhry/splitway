e9bb3212384859bfa0a12a33e6be4c57bb97d9c0
LukeAPearce <lpearce6@gatech.edu>
2019-11-02 17:16:19 -0400

    Implemented Login

---
+++ hackathon/splitway/forms.py
@@ -0,0 +1,31 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from splitway.models import User
+
+
+class RegistrationForm(FlaskForm):
+    first_name = StringField('First Name',
+                           validators=[DataRequired()])
+    last_name = StringField('Last Name',
+                           validators=[DataRequired()])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Create Account')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
