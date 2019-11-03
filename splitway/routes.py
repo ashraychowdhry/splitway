e98ae31bd8bfbb5a74dd3cccc0efc9d63aa506f4
unknown <peytonhowelle@gmail.com>
2019-11-02 23:49:21 -0500

    Fixed search for event errors

--- splitway/routes.py
+++ splitway/routes.py
@@ -19,7 +19,7 @@ def getDistance(address1, address2):
     with urllib.request.urlopen(distanceURL) as url:
         data = json.loads(url.read().decode())
     distance = data["rows"][0]["elements"][0]["distance"]["value"]
-    distance = distance / 1609.344
+    distance = float(distance) / 1609.344
     return distance
 @app.route("/search", methods=['GET', 'POST'])
 def search():
@@ -40,24 +40,28 @@ def search():
         userTime = int(userHour) * 60 + int(userMinute)
         query = Event.query.all()
         for event in query:
-            eventCurrentAddress = event["current_location"]
-            eventDestinationAddress = event["destination"]
-            eventTime = (str)(event["time"])
+            eventCurrentAddress = event.current_address
+            eventDestinationAddress = event.destination_address
+            eventTime = (str)(event.time)
             eventYear = eventTime[0:4]
             eventMonth = eventTime[5:7]
             eventDay = eventTime[8:10]
             eventHour = eventTime[11:13]
             eventMinute = eventTime[14:16]
             eventTime = int(eventHour) * 60 + int(eventMinute)
-            time = abs(eventTime-userTime)
+            time = int(abs(eventTime-userTime))
             currentDistance = getDistance(thisUserCurrenLocation, eventCurrentAddress)
             destinationDistance = getDistance(thisUserDestination, eventDestinationAddress)
-            if currentDistance < .5 and destinationDistance < .5 and time < 15 and eventYear == userYear and eventMonth == userMonth and eventDay == userDay:
+            if currentDistance < 1000 and destinationDistance < 1000 and time < 15 and eventYear == userYear and eventMonth == userMonth and eventDay == userDay:
                 eventList.add(event)
         eventListTruncate = []
+        listSize = len(eventList)
         for i in range(4):
-            eventListTruncate = eventList[i]
-        return render_template('results.html', eventList = evenListTruncated)
+            try:
+                eventListTruncate = eventList[i]
+            except:
+                break
+        return render_template('results.html', eventList = eventListTruncate, listSize = listSize),
     return render_template('search.html', form=form)
 
 @app.route("/register", methods=['GET', 'POST'])
